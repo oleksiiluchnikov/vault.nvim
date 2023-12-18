d0efba54660b9542a066145b987d1133780c3bdf
Oleksii Luchnikov <oleksiiluchnikov@gmail.com>
2023-12-18 04:46:46 +0200

    docs: improve documentation for Vault module
    Improved documentation for the Vault module:
    
    - Added more detailed docstrings for Vault.setup and Vault.checkhealth functions
    - Documented the parameters and return values for these functions
    - Added some high level comments explaining the overall purpose of the module
    - Followed Lua style guide for documentation comments

--- lua/vault/init.lua
+++ lua/vault/init.lua
@@ -1,44 +1,51 @@
 ---@class Vault
----@field setup fun(opts: table?): nil - Setup the vault plugin. 
+---@field setup fun(opts: table?): nil - Setup the vault plugin.
 ---@field checkhealth fun(): nil - Check the health of the vault plugin.
 local Vault = {}
 
---- Setup the vault plugin.
+--- Setup `vault.nvim` plugin.
 ---
----@param opts table? - Configuration options (optional).
+---@param opts VaultConfig.options?
 function Vault.setup(opts)
-  opts = opts or {}
-  require("vault.config").setup(opts)
-  require("vault.commands")
-  require("vault.cmp").setup()
+    local config = require("vault.config")
+    config.setup(opts)
+
+    if config.options.commands then
+        require("vault.commands")
+    end
+
+    if config.options.cmp then
+        require("vault.cmp").setup()
+    end
 end
 
 --- Check the health of the vault plugin.
 ---
 --- This function is used by the `:checkhealth` command.
----@return nil
+---@return table
 function Vault.checkhealth()
-
-  local function format_error(plugin_name)
-    local has = pcall(require, plugin_name)
-    local message = string.format("`%s` is required to run vault.nvim", plugin_name)
-    if not has then
-      return {
-        status = "error",
-        message = message,
-      }
+    ---@param plugin_name string
+    ---@return table|nil
+    local function format_error(plugin_name)
+        local has = pcall(require, plugin_name)
+        local message = string.format("`%s` is required to run vault.nvim", plugin_name)
+        if not has then
+            return {
+                status = "error",
+                message = message,
+            }
+        end
     end
-  end
 
-  if not pcall(require, "telescope") then
-    return format_error("telescope")
-  elseif not pcall(require, "cmp") then
-    return format_error("cmp")
-  end
-  return {
-    status = "ok",
-    message = "All dependencies are installed",
-  }
+    if not pcall(require, "telescope") then
+        return format_error("telescope")
+    elseif not pcall(require, "cmp") then
+        return format_error("cmp")
+    end
+    return {
+        status = "ok",
+        message = "All dependencies are installed",
+    }
 end
 
 return Vault
