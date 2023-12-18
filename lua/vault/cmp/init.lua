a43b2b0d0006f6125627cf8d793dc53726301539
Oleksii Luchnikov <oleksiiluchnikov@gmail.com>
2023-12-18 21:05:59 +0200

    refactor: improve date completion source
    - Extract date pattern to constant
    - Improve variable and function naming
    - Add type annotations and documentation
    - Simplify date parsing logic
    - Handle missing context in completion callback
    - Extract date components from typed string
    - Disable duplicate field warning

--- lua/vault/cmp/init.lua
+++ lua/vault/cmp/init.lua
@@ -1,90 +1,131 @@
 local M = {}
 
-local date_pattern = "%d%d%d%d%-%d%d%-%d%d"
+--- Date pattern string used for date matching.
+local DATE_PATTERN = "%d%d%d%d%-%d%d%-%d%d"
 
 local has_cmp, cmp = pcall(require, "cmp")
 if not has_cmp then
-    error("nvim-cmp is not installed")
+    error("`nvim-cmp` is not installed")
     return
 end
 
 local has_dates, Dates = pcall(require, "dates")
 if not has_dates then
-    error("dates.nvim is not installed")
+    error("`dates.nvim` is not installed")
     return
 end
 
+local function is_available()
+    if vim.bo.filetype == "markdown" then
+        return true
+    end
+end
+
+-- Disable duplicate set field warning
+-- Registering the source requires re-declaring the field
+-- But LSP features like completions and help tags still work
+---@diagnostic disable: duplicate-set-field
+
+--- Register the cmp.Source for the `vault_date` source.
+---@see cmp.Source
 local function register_date_source()
-    local source = {}
+    ---@class cmp.Source
+    local source = {
+        is_available = is_available,
+    }
+
+    ---@return cmp.Source
     source.new = function()
         return setmetatable({}, { __index = source })
     end
 
-    source.is_available = function()
-        if vim.bo.filetype == "markdown" then
-            return true
-        end
-    end
-
+    ---Return trigger characters for triggering completion (optional).
+    ---We use \d, -, and space to trigger completion.
+    ---@return string[]
     source.get_trigger_characters = function()
         return { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", " " }
     end
 
-    source.get_keyword_pattern = function() -- keyword_pattern is used to match the keyword before the cursor
-        return [=[\[\d\-\s\]+$]=]
+    --- Notice that it uses vim regex pattern, not lua regex pattern.
+    source.get_keyword_pattern = function()
+        return [=[\[\d\-\s*\]+$]=]
     end
 
+    ---@param request cmp.Context|table
+    ---@param callback function
     source.complete = function(_, request, callback)
-        local input = request.context.cursor_before_line:sub(request.offset - 1)
-        local typed_date =
-            request.context.cursor_before_line:sub(request.offset - 11, request.offset - 1)
-        local typed_string = request.context.cursor_before_line:match("[%d%-]+$")
+        local context = request.context
+        if not context or type(context) ~= "table" then
+            return
+        end
+        ---@type string
+        local cursor_before_line = request.context.cursor_before_line
 
-        if
-            typed_date and typed_date:match(date_pattern) or typed_date:match(date_pattern .. " ")
-        then
+        local offset = request.offset
+        local date_length = 11
+
+        local input = cursor_before_line:sub(offset - 1)
+        local typed_date = cursor_before_line:sub(offset - date_length, offset - 1)
+        local typed_string = cursor_before_line:match("[%d%-]+$")
+
+        if not typed_date and not typed_string then
+            return
+        end
+
+        if typed_date:match(DATE_PATTERN) or typed_date:match(DATE_PATTERN .. " ") then
+            ---@type lsp.CompletionItem[]
             local items = {}
-            local weekday = os.date(
-                "%A",
-                os.time({
-                    year = typed_date:sub(1, 4),
-                    month = typed_date:sub(6, 7),
-                    day = typed_date:sub(9, 10),
-                })
-            )
+            local year = typed_date:sub(1, 4)
+            local month = typed_date:sub(6, 7)
+            local day = typed_date:sub(9, 10)
+            local time = os.time({
+                year = year,
+                month = month,
+                day = day,
+            })
+            local weekday = os.date("%A", time)
             local new_text = weekday
             if #typed_date == 10 then
                 new_text = " " .. weekday
             end
 
-            table.insert(items, {
+            if type(new_text) ~= "string" then
+                return
+            elseif type(weekday) ~= "string" then
+                return
+            end
+
+            ---@type lsp.CompletionItem
+            local item = {
                 label = weekday,
                 kind = 12,
                 textEdit = {
                     newText = new_text,
                     range = {
                         start = {
-                            line = request.context.cursor.row - 1,
-                            character = request.context.cursor.col - #input,
+                            line = context.cursor.row - 1,
+                            character = context.cursor.col - #input,
                         },
                         ["end"] = {
-                            line = request.context.cursor.row - 1,
-                            character = request.context.cursor.col,
+                            line = context.cursor.row - 1,
+                            character = context.cursor.col,
                         },
                     },
                 },
-            })
+            }
+            table.insert(items, item)
             callback({
                 items = items,
                 isIncomplete = true,
             })
-        elseif typed_string and typed_string:match("%d+") and #typed_string > 2 then
-            local dates = {}
-            dates = Dates.get(typed_string)
+        elseif typed_string:match("%d+") and #typed_string > 2 then
+            ---@type string[]
+            local dates = Dates.get(typed_string)
+            ---@type lsp.CompletionItem[]
             local items = {}
             ---@type VaultConfig.options|VaultConfig
             local config = require("vault.config")
-            local journal_dir = config.dirs.journal.daily
+            local journal_dir = config.options.dirs.journal.daily
             for _, date in ipairs(dates) do
                 local weekday = Dates.get_weekday(date)
                 local path = journal_dir .. "/" .. date .. " " .. weekday .. ".md"
@@ -105,12 +146,12 @@ local function register_date_source()
                         newText = date,
                         range = {
                             start = {
-                                line = request.context.cursor.row - 1,
-                                character = request.context.cursor.col - #typed_string - 1,
+                                line = context.cursor.row - 1,
+                                character = context.cursor.col - #typed_string - 1,
                             },
                             ["end"] = {
-                                line = request.context.cursor.row - 1,
-                                character = request.context.cursor.col,
+                                line = context.cursor.row - 1,
+                                character = context.cursor.col,
                             },
                         },
                     },
@@ -132,19 +173,17 @@ local function register_date_source()
     cmp.register_source("vault_date", source.new())
 end
 
-local function register_tag_source()
-    local Tags = require("vault.tags")
-    local tags = Tags()
-    local source = {}
 
+local function register_tags_source()
+    local tags = require("vault.tags")()
+    ---@class cmp.Source
+    local source = {
+        is_available = is_available,
+    }
+
+    ---@return cmp.Source
     source.new = function()
         return setmetatable({}, { __index = source })
-    end
-
-    source.is_available = function()
-        if vim.bo.filetype == "markdown" then
-            return true
-        end
     end
 
     source.get_trigger_characters = function()
@@ -155,49 +194,62 @@ local function register_tag_source()
         return [=[\%(#\%(\w\|\-\|_\|\/\)\+\)]=]
     end
 
+    ---@param request cmp.Context|table
+    ---@param callback function
     source.complete = function(_, request, callback)
-        local input = request.context.cursor_before_line:sub(request.offset - 1)
-        local prefix = request.context.cursor_before_line:sub(1, request.offset - 1)
+        local offset = request.offset
+        local context = request.context
 
-        if prefix:match("#") then
-            local items = {}
-            for tag_name, tag in pairs(tags.map) do
-                table.insert(items, {
-                    label = tag_name,
-                    kind = 12,
-                    textEdit = {
-                        newText = tag_name,
-                        range = {
-                            start = {
-                                line = request.context.cursor.row - 1,
-                                character = request.context.cursor.col - #input,
-                            },
-                            ["end"] = {
-                                line = request.context.cursor.row - 1,
-                                character = request.context.cursor.col,
-                            },
-                        },
-                    },
-                    -- documentation = {
-                    --     kind = "markdown",
-                    --     value = tag.data.documentation:content(tag.data.name),
-                    -- },
-                })
-            end
-            callback({
-                items = items,
-                isIncomplete = true,
-            })
-        else
-            callback({ isIncomplete = true })
+        local cursor_before_line = context.cursor_before_line
+        local input = cursor_before_line:sub(offset - 1)
+        ---@type string
+        ---| "#"
+        local prefix = cursor_before_line:sub(1, offset - 1)
+
+        if not prefix then
+            return
+        elseif not prefix:match("#") then
+            return
         end
-    end
 
-    cmp.register_source("vault_tag", source.new())
+        ---@type lsp.CompletionItem[]
+        local items = {}
+        for tag_name, _ in pairs(tags.map) do
+            ---@type lsp.CompletionItem
+            local item = {
+                label = tag_name,
+                kind = 14, -- Keyword
+                textEdit = {
+                    newText = tag_name,
+                    range = {
+                        start = {
+                            line = context.cursor.row - 1,
+                            character = context.cursor.col - #input,
+                        },
+                        ["end"] = {
+                        line = context.cursor.row - 1,
+                        character = context.cursor.col,
+                    },
+                },
+            },
+            -- documentation = {
+            --     kind = "markdown",
+            --     value = tag.data.documentation:content(tag.data.name),
+            -- },
+        }
+        table.insert(items, item)
+    end
+    callback({
+        items = items,
+        isIncomplete = true,
+    })
+end
+
+cmp.register_source("vault_tag", source.new())
 end
 
 function M.setup()
-    register_tag_source()
+    register_tags_source()
     register_date_source()
 end
 
